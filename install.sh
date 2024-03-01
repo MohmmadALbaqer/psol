eb61b7c2ef05a7aef9f4fc2e326d9f341da61531
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-03-01 08:21:22 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+    red_sudo=$(tput setaf 1) # Set text color to red
+    echo -e "\[${red_sudo}\]You need to run this program with sudo. Please !.\[${reset}\]"
+    exit 1
+fi
+
+sudo apt-get update 
+sudo apt-get install -y nodejs
+sudo apt-get install openssh-client
+sudo apt-get update
+sudo apt-get install -y npm
