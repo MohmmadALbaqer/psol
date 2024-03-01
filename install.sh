fd25bb59fe1ae422ed94c7868dccf9c773ac733e
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-03-01 08:26:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,14 @@ if [ "$(id -u)" != "0" ]; then
     echo -e "\[${red_sudo}\]You need to run this program with sudo. Please !.\[${reset}\]"
     exit 1
 fi
+clear
+echo '''
+ _           _        _ _ 
+(_)_ __  ___| |_ __ _| | |
+| | '_ \/ __| __/ _` | | |
+| | | | \__ \ || (_| | | |
+|_|_| |_|___/\__\__,_|_|_|
+'''
 
 sudo apt-get update 
 sudo apt-get install -y nodejs
