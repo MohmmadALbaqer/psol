9d65312af8e5e390f6969eca230f4025ee47a09b
محمد الباقر <147254383+MohmmadALbaqer@users.noreply.github.com>
2024-03-02 08:10:38 +0300

    Update install-termux.sh

--- install-termux.sh
+++ install-termux.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+clear
+echo '''
+ _           _        _ _ 
+(_)_ __  ___| |_ __ _| | |
+| | '_ \/ __| __/ _` | | |
+| | | | \__ \ || (_| | | |
+|_|_| |_|___/\__\__,_|_|_|
+'''
+
+apt-get update 
+apt-get install -y nodejs
+apt-get install openssh-client
+apt-get update
+apt-get install -y npm
